8e27eec560a2887d88e443f863a6e3dbaef7d1ec
patrick Carter <patrickcarter1@yahoo.com>
2014-10-06 12:45:50 -0600

    Added chapter_3

---
+++ chapter_3/Readme.md
@@ -0,0 +1,9 @@
+> Above each line, use the # to write a comment to yourself explaining what the line does.
+Completed.
+
+> Remember in Exercise 0 when you started Ruby? 
+Start Ruby this way again and using the math operators, use Ruby as a calculator.
+Find something you need to calculate and write a new .rb file that does it.
+Notice the math seems "wrong"? There are no fractions, only whole numbers. You need to use a "floating point" number, which is a number with a decimal point, as in 10.5, or 0.89, or even 3.0.
+Rewrite ex3.rb to use floating point numbers so it's more accurate. 20.0 is floating point.
+
---
+++ chapter_3/ex3.rb
@@ -0,0 +1,32 @@
+# Prints he is counting chickens
+puts 'I will now count my chickens:'
+
+# uses interpalates math for the number of hens and roosters
+puts 'Hens #{25 + 30 / 6}'
+puts 'Roosters #{100 - 25 * 3 % 4}'
+
+# prints he is counting the eggs
+puts 'Now I will count the eggs:'
+
+# uses math to pring out the nuber of eggs
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# uses the ? to define if the equation he made is true
+puts 'Is it true that 3 + 2 < 5 - 7?'
+
+# will print false cause the equation is not correct
+puts 3 + 2 < 5 - 7
+
+# uses the #{} to interpolate the equation and answer the question
+puts 'What is 3 + 2? #{3 + 2}'
+puts 'What is 5 - 7? #{5 - 7}'
+
+# prints the statement
+puts "Oh, that's why it's false."
+
+# prints the statement
+puts 'How about some more.'
+
+# will answer the question that is being interpolateds
+puts 'Is it greater? #{5 > -2}'
+puts 'Is it less or equal? #{5 <= -2}'
